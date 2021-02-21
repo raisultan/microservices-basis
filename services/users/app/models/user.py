abedfd7a41045718133e3ab1be1fcad832ee047e
raisultan <ki.xbozz@gmail.com>
2021-02-22 04:00:44 +0700

    users: models - user

---
+++ services/users/app/models/user.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Boolean, Column, Integer, String
+
+from app.db.base import Base
+
+
+class User(Base):
+    id = Column(Integer, primary_key=True, index=True)
+    first_name = Column(String, index=True)
+    last_name = Column(String, index=True)
+
+    email = Column(String, unique=True, index=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
+
+    is_active = Column(Boolean(), default=True)
+    is_superuser = Column(Boolean(), default=False)
