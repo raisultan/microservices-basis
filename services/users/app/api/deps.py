f00c9ce9b1753eebf45e3bc42d3267718d6771fe
raisultan <ki.xbozz@gmail.com>
2021-02-22 22:52:51 +0700

    users: api deps refactored

--- services/users/app/api/deps.py
+++ services/users/app/api/deps.py
@@ -1,4 +1,4 @@
-from typing import Generator
+from typing import Generator, Final
 
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
@@ -16,46 +16,57 @@ reusable_oauth2 = OAuth2PasswordBearer(
 )
 
 
-def get_db() -> Generator:
-    try:
-        db = SessionLocal()
-        yield db
-    finally:
-        db.close()
+class DBService:
+    class Error:
+        INVALID_CREDS: Final[str] = 'Could not validate credentials'
+        USER_NOT_FOUND: Final[str] = 'User was not found'
+        INACTIVE_USER: Final[str] = 'User is inactive'
+        NOT_SUPERUSER: Final[str] = 'The user does not have enough privileges'
 
+    @staticmethod
+    def get_db() -> Generator:
+        try:
+            db = SessionLocal()
+            yield db
+        finally:
+            db.close()
 
-def get_current_user(
-    db: Session = Depends(get_db), token: str = Depends(reusable_oauth2)
-) -> models.User:
-    try:
-        payload = jwt.decode(
-            token, settings.SECRET_KEY, algorithms=[AuthService.ALGORITHM]
-        )
-        token_data = schemas.TokenPayload(**payload)
-    except (jwt.JWTError, ValidationError):
-        raise HTTPException(
-            status_code=status.HTTP_403_FORBIDDEN,
-            detail="Could not validate credentials",
-        )
-    user = crud.user.get(db, id=token_data.sub)
-    if not user:
-        raise HTTPException(status_code=404, detail="User not found")
-    return user
+    @classmethod
+    def get_current_user(
+            cls,
+            db: Session = Depends(get_db),
+            token: str = Depends(reusable_oauth2)
+    ) -> models.User:
+        try:
+            payload = jwt.decode(
+                token, settings.SECRET_KEY, algorithms=[AuthService.ALGORITHM]
+            )
+            token_data = schemas.TokenPayload(**payload)
+        except (jwt.JWTError, ValidationError):
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail=cls.Error.INVALID_CREDS,
+            )
 
+        user = crud.user.get(db, id=token_data.sub)
+        if not user:
+            raise HTTPException(status_code=404, detail=cls.Error.USER_NOT_FOUND)
+        return user
 
-def get_current_active_user(
-    current_user: models.User = Depends(get_current_user),
-) -> models.User:
-    if not crud.user.is_active(current_user):
-        raise HTTPException(status_code=400, detail="Inactive user")
-    return current_user
+    @classmethod
+    def get_current_active_user(
+            cls,
+            current_user: models.User = Depends(get_current_user),
+    ) -> models.User:
+        if not crud.user.is_active(current_user):
+            raise HTTPException(status_code=400, detail=cls.Error.INACTIVE_USER)
+        return current_user
 
-
-def get_current_active_superuser(
-    current_user: models.User = Depends(get_current_user),
-) -> models.User:
-    if not crud.user.is_superuser(current_user):
-        raise HTTPException(
-            status_code=400, detail="The user doesn't have enough privileges"
-        )
-    return current_user
+    @classmethod
+    def get_current_active_superuser(
+            cls,
+            current_user: models.User = Depends(get_current_user),
+    ) -> models.User:
+        if not crud.user.is_superuser(current_user):
+            raise HTTPException(status_code=400, detail=cls.Error.NOT_SUPERUSER)
+        return current_user
