97689295c271d6f0a106c5d57d323c33da09c9a3
raisultan <ki.xbozz@gmail.com>
2021-02-22 21:39:11 +0700

    users: endpoints added

--- services/users/app/main.py
+++ services/users/app/main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
-app = FastAPI()
+from app.api.api_v1.api import api_router
+from app.core.config import settings
 
+app = FastAPI(
+    title=settings.PROJECT_NAME, openapi_url=f"{settings.API_V1_STR}/openapi.json"
+)
 
-@app.get('/')
-async def root():
-    return {'message': 'Users app is up and running'}
+app.include_router(api_router, prefix=settings.API_V1_STR)
