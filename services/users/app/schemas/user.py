00352687d2daf68f0bb2f06a279dfa501c5996c8
raisultan <ki.xbozz@gmail.com>
2021-02-22 21:38:48 +0700

    users: schemas adjusted

--- services/users/app/schemas/user.py
+++ services/users/app/schemas/user.py
@@ -7,7 +7,8 @@ class UserBase(BaseModel):
     email: Optional[EmailStr] = None
     is_active: Optional[bool] = True
     is_superuser: bool = False
-    full_name: Optional[str] = None
+    first_name: Optional[str] = None
+    last_name: Optional[str] = None
 
 
 class UserCreate(UserBase):
