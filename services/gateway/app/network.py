6bfa6058e1170485eb5992caee5c6e26b7807d7b
raisultan <ki.xbozz@gmail.com>
2021-02-24 00:32:09 +0700

    gateway: make_request

---
+++ services/gateway/app/network.py
@@ -0,0 +1,21 @@
+import aiohttp
+import async_timeout
+
+from config import settings
+
+
+async def make_request(
+        *,
+        url: str,
+        method: str,
+        data: dict = None,
+        headers: dict = None
+):
+    data = data or {}
+
+    with async_timeout.timeout(settings.GATEWAY_TIMEOUT):
+        async with aiohttp.ClientSession() as session:
+            request = getattr(session, method)
+            async with request(url, json=data, headers=headers) as response:
+                data = await response.json()
+                return (data, response.status)
