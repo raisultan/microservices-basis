e755a80a4086f78f08204da1aa0c832701f08aec
raisultan <ki.xbozz@gmail.com>
2021-02-23 15:59:04 +0700

    gateway: excessive deps removed

--- services/gateway/app/api/dependencies/deps.py
+++ services/gateway/app/api/dependencies/deps.py
@@ -1,12 +1,6 @@
 from typing import Generator
 
-from fastapi import Depends, HTTPException
-from fastapi_jwt_auth import AuthJWT
-from sqlalchemy.orm import Session
-
-from app import crud, models
 from app.db.session import SessionLocal
-from .errors import DBError
 
 
 def get_db() -> Generator:
@@ -15,25 +9,3 @@ def get_db() -> Generator:
         yield db
     finally:
         db.close()
-
-
-def get_current_user(
-        db: Session = Depends(get_db),
-        Authorize: AuthJWT = Depends(),
-) -> models.User:
-    Authorize.jwt_required()
-
-    user_email = Authorize.get_jwt_subject()
-    user = crud.user.get_by_email(db, email=user_email)
-
-    if not user:
-        raise HTTPException(status_code=404, detail=DBError.USER_NOT_FOUND)
-    return user
-
-
-def get_current_active_user(
-        current_user: models.User = Depends(get_current_user),
-) -> models.User:
-    if not crud.user.is_active(current_user):
-        raise HTTPException(status_code=400, detail=DBError.INACTIVE_USER)
-    return current_user
