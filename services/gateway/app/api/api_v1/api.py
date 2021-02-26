b71dde20b9bd4a0283783fa1cecbd10a75dd5706
raisultan <ki.xbozz@gmail.com>
2021-02-26 18:42:59 +0700

    gateway: service_endpoints made distinct

--- services/gateway/app/api/api_v1/api.py
+++ services/gateway/app/api/api_v1/api.py
@@ -2,8 +2,8 @@ from fastapi import APIRouter
 
 from app.config import settings
 from app.api.api_v1.endpoints.auth import routes as auth_routes
-from app.api.api_v1.endpoints.users import routes as users_routes
+from app.api.service_endpoints.users import routes as users_routes
 
 api_router = APIRouter()
-api_router.include_router(auth_routes.router, prefix=f'{settings.API_V1_STR}/auth', tags=['auth'])  # TODO: temp solution
+api_router.include_router(auth_routes.router, prefix=f'{settings.API_V1_STR}/auth', tags=['auth'])
 api_router.include_router(users_routes.router, prefix='', tags=['users'])
